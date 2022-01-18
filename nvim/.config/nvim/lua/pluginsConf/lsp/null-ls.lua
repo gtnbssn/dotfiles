3ea81a1bcc29bbef881d822bbdb70fa62ea0425c
gtnbssn <gaetan.boisson@gmail.com>
2022-01-18 10:51:01 +0800

    use local prettier if available

--- nvim/.config/nvim/lua/pluginsConf/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/pluginsConf/lsp/null-ls.lua
@@ -6,12 +6,14 @@ end
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-local diagnostics = null_ls.builtins.diagnostics
+-- local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier,
+		formatting.prettier.with({
+        prefer_local = "node_modules/.bin",
+    }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
     -- diagnostics.flake8
