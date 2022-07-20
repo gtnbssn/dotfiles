81af1db70d7a7b94c298b8e04810fd02a4b08de3
gtnbssn <gaetan.boisson@gmail.com>
2022-07-20 19:38:06 +0200

    add illuminate

--- nvim/.config/nvim/lua/nvimConf/plugins.lua
+++ nvim/.config/nvim/lua/nvimConf/plugins.lua
@@ -100,6 +100,7 @@ return packer.startup(function(use)
 	use("tamago324/nlsp-settings.nvim") -- language server settings defined in json for
 	use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters
 	use("b0o/schemastore.nvim")
+  use("RRethy/vim-illuminate")
 	require("pluginsConf.lsp")
 
 	-- autotag
