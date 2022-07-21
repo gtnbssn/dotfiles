f27a2a4f566b9053f607d708bc50e8a46bbfd4e7
gtnbssn <gaetan.boisson@gmail.com>
2022-07-21 22:21:04 +0200

    more impatient waxing

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,10 +1,25 @@
 -- adapted from https://github.com/LunarVim/Neovim-from-scratch
-local status_ok, _ = pcall(require, "impatient")
+
+_G.__luacache_config = {
+	chunks = {
+		enable = true,
+		path = vim.fn.stdpath("cache") .. "/luacache_chunks",
+	},
+	modpaths = {
+		enable = true,
+		path = vim.fn.stdpath("cache") .. "/luacache_modpaths",
+	},
+}
+
+local status_ok, impatient = pcall(require, "impatient")
 if not status_ok then
 	vim.notify("impatient did not load", vim.log.levels.WARN)
 	return
+-- else
+--   impatient.enable_profile()
 end
-require "nvimConf.options"
-require "nvimConf.keymaps"
-require "nvimConf.autocommands"
-require "nvimConf.plugins"
+
+require("nvimConf.options")
+require("nvimConf.keymaps")
+require("nvimConf.autocommands")
+require("nvimConf.plugins")
