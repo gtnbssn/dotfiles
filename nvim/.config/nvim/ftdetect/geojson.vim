2e0189d598b7977b9732dd5e6e0bd7093cc31efd
gtnbssn <gaetan.boisson@gmail.com>
2022-07-21 22:42:18 +0200

    detect .geojson as json files, and use the right schema with them

---
+++ nvim/.config/nvim/ftdetect/geojson.vim
@@ -0,0 +1,1 @@
+autocmd BufRead,BufEnter *.geojson set filetype=json
--- nvim/.config/nvim/lua/pluginsConf/lsp/settings/jsonls.lua
+++ nvim/.config/nvim/lua/pluginsConf/lsp/settings/jsonls.lua
@@ -1,9 +1,17 @@
 return {
-		settings = {
-			json = {
-				schemas = require("schemastore").json.schemas(),
-        validate = { enable = true },
-			},
+	settings = {
+		json = {
+			schemas = require("schemastore").json.schemas({
+				replace = {
+					["geojson.json"] = {
+						description = "geojson overriden with a fileMatch thank you",
+						fileMatch = { "*.geojson" },
+						name = "geojson.json",
+						url = "https://json.schemastore.org/geojson.json",
+					},
+				},
+			}),
+			validate = { enable = true },
 		},
-	}
-
+	},
+}
