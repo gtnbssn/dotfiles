2f54adeb82868dda0983a10acea617fcefc83cc7
gtnbssn <gaetan.boisson@gmail.com>
2022-07-21 07:41:12 +0200

    add flake8 and eslint diagnostics + shfmt formatting

--- nvim/.config/nvim/lua/pluginsConf/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/pluginsConf/lsp/null-ls.lua
@@ -7,7 +7,7 @@ end
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
--- local diagnostics = null_ls.builtins.diagnostics
+local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
 	debug = false,
@@ -17,6 +17,8 @@ null_ls.setup({
 		}),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
-		-- diagnostics.flake8
+		formatting.shfmt,
+		diagnostics.flake8,
+    diagnostics.eslint
 	},
 })
