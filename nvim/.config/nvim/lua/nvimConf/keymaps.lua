649f4b13d051d07663a0b4c36cf32f516f60ba41
gtnbssn <gaetan.boisson@gmail.com>
2022-08-06 01:07:27 +0800

    cleanup

--- nvim/.config/nvim/lua/nvimConf/keymaps.lua
+++ nvim/.config/nvim/lua/nvimConf/keymaps.lua
@@ -41,17 +41,9 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
--- Move text up and down
--- keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
--- keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
-
 -- keep things centered when searching
 keymap("n", "n", "nzzzv", opts)
 keymap("n", "N", "Nzzzv", opts)
-
--- quick splits
--- keymap("n", "vs", ":vs<CR>", opts)
--- keymap("n", "sp", ":sp<CR>", opts)
 
 ------------
 -- Visual --
@@ -60,18 +52,3 @@ keymap("n", "N", "Nzzzv", opts)
 -- Stay in visual mode
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
-
--- Move text up and down
--- keymap("v", "<A-j>", ":m .+1<CR>==", opts)
--- keymap("v", "<A-k>", ":m .-2<CR>==", opts)
--- keymap("v", "p", '"_dP', opts)
-
-------------------
--- Visual Block --
-------------------
-
--- Move text up and down
--- keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
--- keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
--- keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
--- keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
