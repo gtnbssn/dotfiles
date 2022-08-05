2f1b7c04c26d47071c91b23eaf0a1fcc9b047069
gtnbssn <gaetan.boisson@gmail.com>
2022-08-06 00:55:58 +0800

    lua style autocommands

--- nvim/.config/nvim/lua/nvimConf/autocommands.lua
+++ nvim/.config/nvim/lua/nvimConf/autocommands.lua
@@ -1,42 +1,80 @@
-vim.cmd([[
-  augroup _general_settings
-    autocmd!
-    autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR> 
-    autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'IncSearch', timeout = 200}) 
-    autocmd BufWinEnter * :set formatoptions-=cro
-    autocmd FileType qf set nobuflisted
-  augroup end
-  augroup CursorLine
-    au!
-    au VimEnter,WinEnter,BufWinEnter * setlocal cursorline
-    au WinLeave * setlocal nocursorline
-  augroup END
-  augroup _git
-    autocmd!
-    autocmd FileType gitcommit setlocal wrap
-    autocmd FileType gitcommit setlocal spell
-  augroup end
-  augroup _markdown
-    autocmd!
-    autocmd FileType markdown setlocal wrap
-    autocmd FileType markdown setlocal spell
-  augroup end
-  augroup _auto_resize
-    autocmd!
-    autocmd VimResized * tabdo wincmd = 
-  augroup end
-  augroup vert_help
-    autocmd!
-    autocmd BufEnter *.txt if &buftype == 'help' | wincmd L | endif
-  augroup END
-]])
---  augroup _alpha
---    autocmd!
---    autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
---  augroup end
+vim.api.nvim_create_augroup("generalSettings", { clear = true })
+vim.api.nvim_create_autocmd(
+	"FileType",
+	{
+    pattern = { "qf", "help", "man", "lspinfo" },
+    command = "nnoremap <silent> <buffer> q :close<cr>",
+    group = "generalSettings"
+  }
+)
+vim.api.nvim_create_autocmd(
+	"TextYankPost",
+	{
+    callback = function()
+      vim.highlight.on_yank()
+    end,
+    group = "generalSettings"
+  }
+)
+vim.api.nvim_create_autocmd(
+  "FileType",
+  {
+    pattern = "qf",
+    callback = function()
+      vim.opt.buflisted = false
+    end,
+    group = "generalSettings"
+  }
+)
 
--- Autoformat
--- augroup _lsp
---   autocmd!
---   autocmd BufWritePre * lua vim.lsp.buf.formatting()
--- augroup end
+vim.api.nvim_create_augroup("cursorLine", { clear = true })
+vim.api.nvim_create_autocmd(
+  {"VimEnter", "WinEnter", "BufWinEnter"},
+  {
+    callback = function()
+      vim.opt_local.cursorline = true
+    end,
+    group = "cursorLine"
+  }
+)
+vim.api.nvim_create_autocmd(
+  "WinLeave",
+  {
+    callback = function()
+      vim.opt_local.cursorline = false
+    end,
+    group = "cursorLine"
+  }
+)
+
+vim.api.nvim_create_augroup("redactional", { clear = true })
+vim.api.nvim_create_autocmd(
+  "FileType",
+  {
+    pattern = {"gitcommit", "markdown"},
+    callback = function()
+      vim.opt_local.wrap = true
+      vim.opt_local.spell = true
+    end,
+    group = "redactional"
+  }
+)
+
+vim.api.nvim_create_augroup("autoResize", { clear = true })
+vim.api.nvim_create_autocmd(
+  "VimResized",
+  {
+    command = "tabdo wincmd =",
+    group = "autoResize"
+  }
+)
+
+vim.api.nvim_create_augroup("vertHelp", { clear = true })
+vim.api.nvim_create_autocmd(
+  "BufEnter",
+  {
+    pattern = "*.txt",
+    command = "if &buftype == 'help' | wincmd L | endif",
+    group = "vertHelp"
+  }
+)
