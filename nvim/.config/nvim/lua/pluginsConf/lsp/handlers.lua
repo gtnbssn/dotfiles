9816f2f25e3f82a4bd3611531dc9a58db1566056
gtnbssn <gaetan.boisson@gmail.com>
2022-09-13 16:44:46 +0800

    null-ls for lua formatting

--- nvim/.config/nvim/lua/pluginsConf/lsp/handlers.lua
+++ nvim/.config/nvim/lua/pluginsConf/lsp/handlers.lua
@@ -66,9 +66,9 @@ M.on_attach = function(client, bufnr)
 	if client.name == "tsserver" then
 		client.resolved_capabilities.document_formatting = false
 	end
-	--[[ if client.name == "sumneko_lua" then
+	if client.name == "sumneko_lua" then
 		client.resolved_capabilities.document_formatting = false
-	end ]]
+	end
 	lsp_keymaps(bufnr)
 end
 
